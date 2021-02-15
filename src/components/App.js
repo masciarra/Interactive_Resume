75bff2d8dfa32376807664b2df5c41ac19ba4db7
Max Sciarra <maxsciarra@Maxs-MacBook-Pro.local>
2021-02-15 15:32:14 -0800

    initial upload, create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Welcome from './Welcome'
+import Landing from './Landing'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <Landing />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styling/Landing.css'
+export default function Landing() {
+    return (
+        <div>
+            <p>Landing</p>
+        </div>
+    )
+}
